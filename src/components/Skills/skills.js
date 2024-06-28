faf7171d88cdfd50e1f76a7a669983982eb0282c
Mthokozisi Nxumalo <117337975+mth6k6@users.noreply.github.com>
2024-06-28 11:18:05 +0200

    update

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -12,29 +12,30 @@ export const Skills = () => {
             <div className="skillsIcons">
                 <div className="skillsIcon">
                     <img src={frontend} alt='frontend' className='skillsIconImg'/>
-                    <div className='skillsIconText'>
+                    <div className="skillsIconText">
                     <h2>Front-End Development</h2>
                         <p>React js, Angular, CSS, HTML, Boostrap and Javascript</p>
-                    </div>
-                        
+                    </div>     
                 </div>
             </div>
+
             <div className="skillsIcons">
                 <div className="skillsIcon">
                     <img src={backend} alt='backend' className='skillsIconImg'/>
-                    <div className='skillsIconText'>
+                    <div className="skillsIconText">
                     <h2>Back-End Development</h2>
-                        <p>Node js, MySQL, ASP.NET, Azure Portal and SQL Management Server</p>
-                    </div>
-                       
+                    <p>Node js, MySQL, ASP.NET, Azure Portal and SQL Management Server</p>
+                    </div> 
                 </div>
             </div> 
+
             <div className="skillsIcons">
                 <div className="skillsIcon">
                     <img src={support} alt='support' className='skillsIconImg'/>
-                    <div className='skillsIconText'> <h2>IT Support</h2>
-                        <p>Desktop Technician</p></div>
-                       
+                    <div className="skillsIconText"> 
+                    <h2>IT Support</h2>
+                    <p>Desktop Technician</p>
+                    </div> 
                 </div>
             </div>
 
